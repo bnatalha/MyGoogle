0bd1766802a12710625b9da598b469c8b212d39a
natoalha <nataliabrit0@outlook.com>
2017-11-18 20:49:47 -0300

    protótipo da classe MyIndexItem

---
+++ src/domain/MyIndexItem.java
@@ -0,0 +1,77 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class MyIndexItem
+{
+	/**
+	 * Palavra indexada 	 
+	 */
+	private String word;
+	
+	/**
+	 * Armazena um conjunto de informações referentes aos arquivos fonte onde este item foi encontrado.
+	 */
+	private ArrayList<OcurrenceFile> sources;
+	
+	/**
+	 * 
+	 */
+	public MyIndexItem(String word)
+	{
+		this.word = word;
+		this.sources = new ArrayList<OcurrenceFile>();
+	}
+	
+	/**
+	 * Adiciona uma ocorrencia do item na linha 'line' do arquivo 'filename'.
+	 * @param filename
+	 * @param line
+	 */
+	public void addOcurrenceAt(String f, int line)
+	{
+		
+	}
+	
+	/**
+	 * Lugares onde podem ser encontrados a palavra indexada
+	 */
+	public class OcurrenceFile
+	{
+		
+		/**
+		 * Nome do arquivo referente as ocorrências
+		 */
+		private final String filename;
+				
+		/**
+		 * Linhas onde a palavra foi encontrada
+		 */		
+		private ArrayList<Integer> ocurrences;
+		
+		/**
+		 * Constrói um MyIndexItem para um arquivo .txt
+		 * @param filename nome do arquivo .txt
+		 */
+		public OcurrenceFile(String filename)
+		{
+			this.filename = filename;
+			this.ocurrences = new ArrayList<Integer>();
+		}
+
+		/**
+		 * @return the filename
+		 */
+		public String getFilename() {
+			return filename;
+		}
+		
+		/**
+		 * @return the ocurrences
+		 */
+		public ArrayList<Integer> getOcurrences() {
+			return ocurrences;
+		}
+	}	
+	
+}
