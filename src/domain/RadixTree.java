2b138bc81a20eabcb72f7e7aabfeeac3ee070e3b
natoalha <nataliabrit0@outlook.com>
2017-11-18 17:31:19 -0300

    add 1º versão não testada do search e remove

--- src/domain/RadixTree.java
+++ src/domain/RadixTree.java
@@ -21,16 +21,7 @@ public class RadixTree
 	private final int ALPHABET_SIZE;
 	
 	// ================================================ METODOS ================================================
-	
-	/**
-	 * Constrói uma árvore radix vazio com o alfbeto de tamanho 26.
-	 */
-	public RadixTree() 
-	{
-		this.ALPHABET_SIZE = 26;
-		this.root = new RadixNode(); 
-	}
-	
+		
 	/**
 	 * Constrói uma árvore radix vazia com o alfbeto de tamanho N.
 	 * @param N tamanho do alfabeto dessa árvore.
@@ -38,7 +29,179 @@ public class RadixTree
 	public RadixTree(int N) 
 	{
 		this.ALPHABET_SIZE = N;
-		this.root = new RadixNode(); 
+		this.root = new RadixNode(N); 
+	}
+	
+	/**
+	 * Calcula a posição em que o nó filho com o prefixo que combina com 'str' deve estar.
+	 * @param str String a ser analizada.
+	 * @return o mod do valor na tabela ascii da primeira letra de 'str' pelo valor de 'a' na mesma tabela.
+	 * @see http://www.asciitable.com/
+	 */
+	private int getPosition(String str)
+	{
+		int divisor = (int) 'a';
+		return ( ((int)(str.charAt(0))) % divisor);
+	}
+	
+	/**
+	 * Procura por uma palavra na árvore.
+	 * @param str palavra a ser procurada.
+	 * @return 'true' caso a palavra seja encontrada.
+	 */
+	public boolean searchWord(String str)
+	{
+		if(str != null)
+		{
+			return searchW(root,str);
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	private int getDiffCharIndex(String a, String b)
+	{
+		int i = 0;
+		while (i != a.length() && i != b.length() )
+		{
+			if(a.charAt(i) != b.charAt(i)) break;
+		}
+		
+		return i;
+	}
+	
+	/**
+	 * Busca recursiva
+	 * @param n
+	 * @param str
+	 * @return
+	 */
+	private boolean searchW(RadixNode n, String str)
+	{
+		if (n != null)
+		{
+			RadixNode suitable_child = n.getChildAt(getPosition(str));
+			if( suitable_child == null)
+			{
+				return false;				
+			}
+			else
+			{
+				int separator = getDiffCharIndex(str,suitable_child.getLabel());
+				if( separator < str.length() && separator == suitable_child.getLength() )
+				{
+					str = str.substring(separator, str.length()); //?
+					searchW(suitable_child,str);
+				}
+				else if( separator < str.length() && separator < suitable_child.getLength() )
+				{
+					return false;	
+				}
+				else if( separator == str.length() && separator < suitable_child.getLength() )
+				{
+					return false;
+				}
+				else if( separator == str.length() && separator == suitable_child.getLength() )
+				{
+					return suitable_child.isEndOfString();
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param s
+	 */
+	public void insertWord(String str)
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @param s
+	 */
+	private void insertW(RadixNode n, String str)
+	{
+		if (n != null)
+		{
+			RadixNode suitable_child = n.getChildAt(getPosition(str));
+			if( suitable_child == null)
+			{
+				RadixNode newchild = new RadixNode(ALPHABET_SIZE,str);
+				n.addChild(newchild);				
+			}
+			else
+			{
+				int separator = getDiffCharIndex(str,suitable_child.getLabel());
+				if( separator < str.length() && separator == suitable_child.getLength() )
+				{
+					str = str.substring(separator); //?
+					insertW(suitable_child,str);
+				}
+				else if( separator < str.length() && separator < suitable_child.getLength() )
+				{
+					// corta a string a ser inserida para ser apenas a parte q tem seu sufixo diferenciado
+					str = str.substring(separator);
+					// cria uma nova string com a parte diferente pertencente ao label do nó analizado
+					String newLabel = suitable_child.getLabel().substring(separator);
+					
+					//Cria um nó com o label cortado do nó original
+					RadixNode newNode = new RadixNode(ALPHABET_SIZE,newLabel);	
+					
+					//move os filhos do nó analizado para o novo nó
+					newNode.setChilds(suitable_child.getChilds());
+					// faz com que o novo nó indica o final de uma string caso o nó de onde ele originou seja.
+					newNode.setEndOfString(suitable_child.isEndOfString());
+					
+					// cria um novo array de filhos vazio para o nó original
+					suitable_child.setChilds(new RadixNode[ALPHABET_SIZE]);
+					// modifica sua label para o prefixo em comum 
+					suitable_child.setLabel(suitable_child.getLabel().substring(0,separator));
+					// seta endOfString para falso
+					suitable_child.setEndOfString(false);
+					
+					//adiciona o novo nó e o nó da string a ser inserida como filhos do original
+					suitable_child.addChild(newNode);
+					suitable_child.addChild(new RadixNode(ALPHABET_SIZE,str));
+				}
+				else if( separator == str.length() && separator < suitable_child.getLength() )
+				{
+					// cria uma nova string com o sufixo diferente pertencente ao label do nó original
+					String newLabel = suitable_child.getLabel().substring(separator);
+					
+					// Cria um nó com o label cortado do nó original
+					RadixNode newNode = new RadixNode(ALPHABET_SIZE,newLabel);	
+					
+					// move os filhos do nó analizado para o novo nó
+					newNode.setChilds(suitable_child.getChilds());
+					// faz com que o novo nó indica um final de uma string caso o original seja.
+					newNode.setEndOfString(suitable_child.isEndOfString());
+					
+					// cria um novo array de filhos vazio para o nó original
+					suitable_child.setChilds(new RadixNode[ALPHABET_SIZE]);
+					// modifica a label do nó original para o prefixo em comum 
+					suitable_child.setLabel(suitable_child.getLabel().substring(0,separator));
+					// seta endOfString para verdadeiro
+					suitable_child.setEndOfString(true);
+					
+					//adiciona o novo nó como filho do original
+					suitable_child.addChild(newNode);
+				}
+				else if( separator == str.length() && separator == suitable_child.getLength() )
+				{
+					suitable_child.setEndOfString(true);
+				}
+			}
+		}
 	}
 	
 	// ================================================ NÓ ================================================
@@ -55,14 +218,19 @@ public class RadixTree
 		private String label;
 		
 		/**
+		 * Peso desse nó. Indica em que posição do array de filhos ele deve ser guardado.
+		 */
+		private int ranking;
+		
+		/**
 		 * Armazena os filhos desse nó
 		 */
-		private ArrayList<RadixNode> childs;
+		private RadixNode[] childs;
 		
 		/**
 		 * Guarda o número máximo de filhos que um nó pode ter.
 		 */
-		private int MAX_CHILDS = ALPHABET_SIZE;
+		private final int MAX_CHILDS;
 		
 		/**
 		 * Indica se esse nó é o fim de uma string.
@@ -76,29 +244,39 @@ public class RadixTree
 		
 		
 		/**
-		 * Constrói um nó vazio.
+		 * Constrói um nó vazio de uma árvore de alfabeto de tamanho 'N'.
 		 */
-		public RadixNode()
+		public RadixNode(int N)
 		{
+			MAX_CHILDS = N;
 			this.endOfString = false;
-			this.childs = new ArrayList<RadixNode>(MAX_CHILDS);
+			this.childs = new RadixNode[MAX_CHILDS];
 		}
 		
 		/**
-		 * Constrói um nó com um label.
+		 * Constrói um nó de uma árvore com o alfabeto de tamanho 'N', já com um label.
 		 */
-		public RadixNode(String label)
+		public RadixNode(int N, String label)
 		{
+			MAX_CHILDS = N;
 			this.endOfString = false;
 			this.label = label;
-			this.childs = new ArrayList<RadixNode>(MAX_CHILDS);
+			updateRanking();
+			this.childs = new RadixNode[MAX_CHILDS];
 		}
-
+		
 		/**
 		 * @return the label
 		 */
 		public String getLabel() {
 			return label;
+		}
+		
+		/**
+		 * @return the length of the label
+		 */
+		public int getLength() {
+			return label.length();
 		}
 
 		/**
@@ -106,19 +284,37 @@ public class RadixTree
 		 */
 		public void setLabel(String label) {
 			this.label = label;
+			updateRanking();
+		}
+		
+		/**
+		 * Atualiza o peso/ranking desse nó.
+		 */
+		private void updateRanking()
+		{
+			int divisor = (int) 'a';
+			ranking = ( ((int)(label.charAt(0))) % divisor);
+		}
+		
+		/**
+		 * @return the ranking
+		 */
+		public int getRanking()
+		{
+			return ranking;
 		}
 
 		/**
 		 * @return the childs
 		 */
-		public ArrayList<RadixNode> getChilds() {
+		public RadixNode[] getChilds() {
 			return childs;
 		}
 
 		/**
 		 * @param childs the childs to set
 		 */
-		public void setChilds(ArrayList<RadixNode> childs) {
+		public void setChilds(RadixNode[] childs) {
 			this.childs = childs;
 		}
 		
@@ -148,6 +344,19 @@ public class RadixTree
 		 */
 		public void setMyIndexItem(MyIndexItem indexitem) {
 			this.indexitem = indexitem;
-		}		
+		}
+		
+		/**
+		 * @param index posição do nó filho
+		 * @return nó filho da posição 'index' caso ele exista.
+		 */
+		public RadixNode getChildAt(int index) {
+			return this.childs[index];
+		}
+		
+		public void addChild(RadixNode n)
+		{
+			childs[n.getRanking()] = n;
+		}
 	}
 }
